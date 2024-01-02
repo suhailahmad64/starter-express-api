4a4830563df1fb454c49186a96fe832cb8670d36
suhailahmad64 <98821344+suhailahmad64@users.noreply.github.com>
2024-01-02 12:44:56 +0500

    puppeteer within express

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
+const puppeteer = require('puppeteer');
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
-// Define a route
-app.get('/', (req, res) => {
-  //const url = 'https://amazon.com';
-  fetchData('https://selfstudyhelp.com');
-    res.send('result above');
-});
+app.get('/', async (req, res) => {
+  const url = 'https://selfstudyhelp.com';
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
-
-const puppeteer = require('puppeteer');
-
-
-async function fetchData(url) {
   // Launch a headless browser
-  const browser = await puppeteer.launch({'headless':'new'});
+  const browser = await puppeteer.launch();
 
   // Create a new page
   const page = await browser.newPage();
@@ -31,16 +20,17 @@ async function fetchData(url) {
     // Get the page content (HTML source code)
     const pageContent = await page.content();
 
-    // Display the HTML source code
-    console.log(pageContent);
-    return pageContent;
-
+    // Send the HTML source code as the response to the client
+    res.send(pageContent);
   } catch (error) {
     console.error('Error fetching data:', error);
+    res.status(500).send('Internal Server Error');
   } finally {
     // Close the browser
     await browser.close();
   }
-}
+});
 
-
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
