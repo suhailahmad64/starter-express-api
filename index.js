1d3d2822de301bc938a8fe0f813692d023a1f3f5
suhailahmad64 <98821344+suhailahmad64@users.noreply.github.com>
2024-01-02 10:43:14 +0500

    simple with puppeteer

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+const puppeteer = require('puppeteer');
+
+
+async function fetchData(url) {
+  // Launch a headless browser
+  const browser = await puppeteer.launch({'headless':'new'});
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  try {
+    // Navigate to the specified URL
+    await page.goto(url);
+
+    // Get the page content (HTML source code)
+    const pageContent = await page.content();
+
+    // Display the HTML source code
+    console.log(pageContent);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  } finally {
+    // Close the browser
+    await browser.close();
+  }
+}
